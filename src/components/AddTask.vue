f66277860cec08b92edde07257de9329a4bc35c5
Giorgi Chagelishvili <chagx116@gmail.com>
2023-10-06 11:58:55 +0400

    create addtask component

---
+++ src/components/AddTask.vue
@@ -0,0 +1,63 @@
+<template>
+  <form class="add-form">
+    <div class="form-control">
+      <label for="text">Task</label>
+      <input type="text" name="text" id="text" placeholder="Add Task" />
+    </div>
+
+    <div class="form-control">
+      <label for="day">Day & Time</label>
+      <input type="text" name="day" id="day" placeholder="Add Day & Time" />
+    </div>
+
+    <div class="form-control form-control-check">
+      <label for="reminder">Set Reminder</label>
+      <input type="checkbox" name="reminder" id="reminder" />
+    </div>
+
+    <input type="submit" value="Save Task" class="btn btn-block" />
+  </form>
+</template>
+
+<script>
+export default {
+  name: "AddTask",
+};
+</script>
+
+<style scoped>
+.add-form {
+  margin-bottom: 40px;
+}
+
+.form-control {
+  margin: 20px 0;
+}
+
+.form-control label {
+  display: block;
+}
+
+.form-control input {
+  width: 100%;
+  height: 40px;
+  margin: 5px;
+  padding: 3px 7px;
+  font-size: 17px;
+}
+
+.form-control-check {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.form-control-check label {
+  flex: 1;
+}
+
+.form-control-check input {
+  flex: 2;
+  height: 20px;
+}
+</style>
